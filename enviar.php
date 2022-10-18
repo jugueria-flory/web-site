68a9e4172f5002fa0ddc5a25446775a2acf23379
web-develomentp <116046824+web-develomentp@users.noreply.github.com>
2022-10-18 06:11:34 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -19,27 +19,4 @@ $carta .= "Mensaje: $mensaje";
 mail($destinatario, $asunto, $carta);
 header('Location:mensaje-de-envio.html');
 
-// Enviado la seccion 
-
-// Llamando a los campos
-$correo = $_POST['correos'];
-
-// Datos para el correo
-$destinatario = "intiprda@gmail.com";
-$asunto = "Suscripción desde nuestra web";
-$mensaje = "Desea suscribirse a tu web"
-
-$carta .= "Correo: $correo \n";
-$carta .= "Mensaje: $mensaje";
-
-// Enviando Mensaje
-mail($destinatario, $asunto, $carta);
-echo"<script> 
-        alert('Se ha suscrito correctamente,¡Gracias!')
-    </script>
-"
-echo"<script> 
-        setTimeout(\"locatio.href='index.html#sabores'\", 3000)
-    </script>
-"
 ?>
